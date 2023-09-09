5d05c96098f0ad971c3fff191be618779b4ca2a3
Lawrence Aritao <lawrence.aritao@gmail.com>
2023-09-09 23:09:42 +0000

    styled

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
-
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Dockerized Go!")
+	// Define the data for the template
+	data := struct {
+		Title   string
+		Message string
+	}{
+		Title:   "Hello, Dockerized Go!",
+		Message: "This is a Go web app with HTML templating.",
+	}
+
+	// Try inline parsing for troubleshooting
+	templateContent := `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Go!</title>
+    <script>console.log("hello")</script>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    <p>{{.Message}}</p>
+</body>
+</html>
+`
+	tmpl, err := template.New("index").Parse(templateContent)
+	if err != nil {
+		http.Error(w, "Failed to parse template", http.StatusInternalServerError)
+		log.Println("Error:", err)  // Log the error
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Println("Error executing template:", err) // Log template execution error
+		return
+	}
+
+	log.Println("Success: Served request from", r.RemoteAddr)  // Log a successful request
 }
 
 func main() {
